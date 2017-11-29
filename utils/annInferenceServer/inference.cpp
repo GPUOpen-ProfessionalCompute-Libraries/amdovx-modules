721d5ac0a8bbe23f34dbb58f931565fdb00280ad
rrawther <Rajy.MeeyakhanRawther@amd.com>
2017-11-28 17:26:31 -0800

    SSE optimization for RGB image resize (#133)

--- utils/annInferenceServer/inference.cpp
+++ utils/annInferenceServer/inference.cpp
@@ -7,7 +7,7 @@
 #include <opencv2/opencv.hpp>
 #include <highgui.h>
 
-#if USE_SSE_FORMAT_CONVERSION
+#if USE_SSE_OPTIMIZATION
 #if _WIN32
 #include <intrin.h>
 #else
@@ -156,10 +156,11 @@ vx_status InferenceEngine::DecodeScaleAndConvertToTensor(vx_size width, vx_size 
 {
     int length = width*height;
     cv::Mat matOrig = cv::imdecode(cv::Mat(1, size, CV_8UC1, inp), CV_LOAD_IMAGE_UNCHANGED);
-    cv::Mat matScaled;
-    cv::resize(matOrig, matScaled, cv::Size(width, height));
 
-#if USE_SSE_FORMAT_CONVERSION
+#if USE_SSE_OPTIMIZATION
+    unsigned char *data_resize = new unsigned char[length * 3];
+    RGB_resize(matOrig.data, data_resize, matOrig.cols, matOrig.rows, width, height);
+
     __m128i mask_B, mask_G, mask_R;
     if (reverseInputChannelOrder)
     {
@@ -173,12 +174,12 @@ vx_status InferenceEngine::DecodeScaleAndConvertToTensor(vx_size width, vx_size 
         mask_G = _mm_setr_epi8((char)0x1, (char)0x80, (char)0x80, (char)0x80, (char)0x4, (char)0x80, (char)0x80, (char)0x80, (char)0x7, (char)0x80, (char)0x80, (char)0x80, (char)0xA, (char)0x80, (char)0x80, (char)0x80);
         mask_B = _mm_setr_epi8((char)0x2, (char)0x80, (char)0x80, (char)0x80, (char)0x5, (char)0x80, (char)0x80, (char)0x80, (char)0x8, (char)0x80, (char)0x80, (char)0x80, (char)0xB, (char)0x80, (char)0x80, (char)0x80);
     }
+    unsigned char * img = data_resize;
     int alignedLength = length& ~3;
     float * B_buf = buf;
     float * G_buf = B_buf + length;
     float * R_buf = G_buf + length;
     int i = 0;
-    unsigned char * img = matScaled.data;
 
     __m128 fR, fG, fB;
     for (; i < alignedLength; i += 4)
@@ -205,6 +206,9 @@ vx_status InferenceEngine::DecodeScaleAndConvertToTensor(vx_size width, vx_size 
         R_buf[i] = (img[2] * preprocessMpy[2]) + preprocessAdd[2];
     }
 #else
+    cv::Mat matScaled;
+    cv::resize(matOrig, matScaled, cv::Size(width, height));
+    float *ptr = buf;
     for (int c = 0; c < 3; c++, ptr += length) {
         float a = preprocessMpy[c], b = preprocessAdd[c];
         unsigned char * img = matScaled.data + (reverseInputChannelOrder ? c : (2 - c));
@@ -212,11 +216,163 @@ vx_status InferenceEngine::DecodeScaleAndConvertToTensor(vx_size width, vx_size 
             ptr[i] = *img * a + b;
         }
     }
+    matScaled.release();
 #endif
     matOrig.release();
-    matScaled.release();
     return VX_SUCCESS;
 }
+
+#define FP_BITS     16
+#define FP_MUL      (1<<FP_BITS)
+
+void InferenceEngine::RGB_resize(unsigned char *Rgb_in, unsigned char *Rgb_out, unsigned int swidth, unsigned int sheight, unsigned int dwidth, unsigned int dheight)
+{
+    float xscale = (float)((double)swidth / (double)dwidth);
+    float yscale = (float)((double)sheight / (double)dheight);
+    // generate xmap
+    int alignW = (dwidth + 15)&~15;
+    unsigned int *Xmap = new unsigned int[alignW*2];
+    unsigned short *Xf = (unsigned short *)(Xmap + alignW);
+    unsigned short *Xf1 = Xf + alignW;
+
+    int xpos = (int)(FP_MUL * (xscale*0.5 - 0.5));
+    int xinc = (int)(FP_MUL * xscale);
+    int yinc = (int)(FP_MUL * yscale);		// to convert to fixed point
+    for (unsigned int x = 0; x < dwidth; x++, xpos += xinc)
+    {
+        int xf;
+        int xmap = (xpos >> FP_BITS);
+        if (xmap >= (int)(swidth - 1)){
+            Xmap[x] = (swidth - 1)*3;
+        }
+        else
+            Xmap[x] = xmap*3;
+        xf = ((xpos & 0xffff) + 0x80) >> 8;
+        Xf[x] = xf;
+        Xf1[x] = (0x100 - xf);
+    }
+    int stride = swidth * 3;
+    int dstride = dwidth * 3;
+    unsigned int aligned_width = dwidth & ~3;	// nearest multiple of 4
+    int ypos = (int)(FP_MUL * (yscale*0.5 - 0.5));
+
+    for (int y = 0; y < (int)dheight; y++, ypos += yinc)
+    {
+        int ym, fy, fy1;
+        unsigned char *pSrc1, *pSrc2;
+        unsigned char *pdst = Rgb_out + y*dstride;
+
+        ym = (ypos >> FP_BITS);
+        fy = ((ypos & 0xffff) + 0x80) >> 8;
+        fy1 = (0x100 - fy);
+        if (ym >= (int)(sheight - 1)){
+            pSrc1 = pSrc2 = Rgb_in + (sheight - 1)*stride;
+        }
+        else
+        {
+            pSrc1 = Rgb_in + ym*stride;
+            pSrc2 = pSrc1 + stride;
+        }
+        __m128i w_y = _mm_setr_epi32(fy1, fy, fy1, fy);
+        const __m128i mm_zeros = _mm_setzero_si128();
+        const __m128i mm_round = _mm_set1_epi32((int)0x80);
+        __m128i p01, p23, ps01, ps23, pRG1, pRG2, pRG3;
+        unsigned int x = 0;
+        for (; x < aligned_width; x += 4)
+        {
+            // load 2 pixels each
+            p01 = _mm_loadl_epi64((const __m128i*) &pSrc1[Xmap[x]]);
+            p23 = _mm_loadl_epi64((const __m128i*) &pSrc1[Xmap[x+1]]);
+            ps01 = _mm_loadl_epi64((const __m128i*) &pSrc2[Xmap[x]]);
+            ps23 = _mm_loadl_epi64((const __m128i*) &pSrc2[Xmap[x + 1]]);
+            // unpcklo for p01 and ps01
+            p01 = _mm_unpacklo_epi8(p01, ps01);
+            p23 = _mm_unpacklo_epi8(p23, ps23);
+            p01 = _mm_unpacklo_epi16(p01, _mm_srli_si128(p01, 6));     //R0R1R2R3 G0G1G2G3 B0B1B2B3 XXXX for first pixel
+            p23 = _mm_unpacklo_epi16(p23, _mm_srli_si128(p23, 6));      //R0R1R2R3 G0G1G2G3 B0B1B2B3 XXXX for second pixel
+
+            // load xf and 1-xf
+            ps01 = _mm_setr_epi32(Xf1[x], Xf1[x], Xf[x], Xf[x]);			// xfxfxf1xf1
+            ps01 = _mm_mullo_epi32(ps01, w_y);                      // W0W1W2W3 for first pixel
+            ps23 = _mm_setr_epi32(Xf1[x + 1], Xf1[x + 1], Xf[x + 1], Xf[x + 1]);
+            ps23 = _mm_mullo_epi32(ps23, w_y);                      // W0W1W2W3 for second pixel
+            ps01 = _mm_srli_epi32(ps01, 8);                 // convert to 16bit
+            ps23 = _mm_srli_epi32(ps23, 8);                 // convert to 16bit
+            ps01 = _mm_packus_epi32(ps01, ps01);                 // convert to 16bit
+            ps23 = _mm_packus_epi32(ps23, ps23);                 // convert to 16bit
+
+            // extend to 16bit
+            pRG1 = _mm_unpacklo_epi8(p01, mm_zeros);        // R0R1R2R3 and G0G1G2G3
+            p01 = _mm_srli_si128(p01, 8);             // B0B1B2B3xxxx
+            p01 = _mm_unpacklo_epi32(p01, p23);       // B0B1B2B3 R0R1R2R3: ist and second
+            p23 = _mm_srli_si128(p23, 4);             // G0G1G2G3 B0B1B2B3 for second pixel
+            p01 = _mm_unpacklo_epi8(p01, mm_zeros);         // B0B1B2B3 R0R1R2R3
+            pRG2 = _mm_unpacklo_epi8(p23, mm_zeros);        // G0G1G2G3 B0B1B2B3 for second pixel
+
+            pRG1 = _mm_madd_epi16(pRG1, ps01);                  // (W0*R0+W1*R1), (W2*R2+W3*R3), (W0*G0+W1*G1), (W2*G2+W3*G3)
+            pRG2 = _mm_madd_epi16(pRG2, ps23);                  //(W0*R0+W1*R1), (W2*R2+W3*R3), (W0*G0+W1*G1), (W2*G2+W3*G3) for seond pixel
+            ps01 = _mm_unpacklo_epi64(ps01, ps23);
+            p01 = _mm_madd_epi16(p01, ps01);                  //(W0*B0+W1*B1), (W2*B2+W3*B3), (W0*R0+W1*R1), (W2*R2+W3*R3) 1st and second pixel
+
+            pRG1 = _mm_hadd_epi32(pRG1, p01);      // R0,G0, B0, R1 (32bit)
+            p01 = _mm_loadl_epi64((const __m128i*) &pSrc1[Xmap[x+2]]);
+            p23 = _mm_loadl_epi64((const __m128i*) &pSrc1[Xmap[x+3]]);
+            ps01 = _mm_loadl_epi64((const __m128i*) &pSrc2[Xmap[x+2]]);
+            ps23 = _mm_loadl_epi64((const __m128i*) &pSrc2[Xmap[x+3]]);
+            pRG1 = _mm_add_epi32(pRG1, mm_round);
+            // unpcklo for p01 and ps01
+            p01 = _mm_unpacklo_epi8(p01, ps01);
+            p01 = _mm_unpacklo_epi16(p01, _mm_srli_si128(p01, 6));     //R0R1R2R3 G0G1G2G3 B0B1B2B3 XXXX for first pixel
+            p23 = _mm_unpacklo_epi8(p23, ps23);
+            p23 = _mm_unpacklo_epi16(p23, _mm_srli_si128(p23, 6));      //R0R1R2R3 G0G1G2G3 B0B1B2B3 XXXX for second pixel
+            // load xf and 1-xf
+            ps01 = _mm_setr_epi32(Xf1[x+2], Xf1[x+2], Xf[x+2], Xf[x+2]);			// xfxfxf1xf1
+            ps01 = _mm_mullo_epi32(ps01, w_y);                      // W0W1W2W3 for first pixel
+            ps23 = _mm_setr_epi32(Xf1[x + 3], Xf1[x + 3], Xf[x + 3], Xf[x + 3]);
+            ps23 = _mm_mullo_epi32(ps23, w_y);                      // W0W1W2W3 for second pixel
+            ps01 = _mm_srli_epi32(ps01, 8);                 // convert to 16bit
+            ps23 = _mm_srli_epi32(ps23, 8);                 // convert to 16bit
+            ps01 = _mm_packus_epi32(ps01, ps01);                 // convert to 16bit
+            ps23 = _mm_packus_epi32(ps23, ps23);                 // convert to 16bit
+            // extend to 16bit
+            pRG3 = _mm_unpacklo_epi8(p01, mm_zeros);        // R0R1R2R3 and G0G1G2G3
+            p01 = _mm_srli_si128(p01, 8);             // B0B1B2B3xxxx
+            p01 = _mm_unpacklo_epi32(p01, p23);       // B0B1B2B3 R0R1R2R3: ist and second
+            p23 = _mm_srli_si128(p23, 4);             // G0G1G2G3 B0B1B2B3 for second pixel
+            p01 = _mm_unpacklo_epi8(p01, mm_zeros);         // B0B1B2B3 R0R1R2R3
+            p23 = _mm_unpacklo_epi8(p23, mm_zeros);        // G0G1G2G3 B0B1B2B3 for second pixel
+
+            pRG3 = _mm_madd_epi16(pRG3, ps01);                  // (W0*R0+W1*R1), (W2*R2+W3*R3), (W0*G0+W1*G1), (W2*G2+W3*G3)
+            p23 = _mm_madd_epi16(p23, ps23);                  //(W0*R0+W1*R1), (W2*R2+W3*R3), (W0*G0+W1*G1), (W2*G2+W3*G3) for seond pixel
+            ps01 = _mm_unpacklo_epi64(ps01, ps23);
+            p01 = _mm_madd_epi16(p01, ps01);                  //(W0*B0+W1*B1), (W2*B2+W3*B3), (W0*B0+W1*B1), (W2*B2+W3*B3) for seond pixel
+
+            pRG2 = _mm_hadd_epi32(pRG2, pRG3);      // G1, B1, R2,G2 (32bit)
+            p01 = _mm_hadd_epi32(p01, p23);      // B2,R3, G3, B3 (32bit)
+            pRG2 = _mm_add_epi32(pRG2, mm_round);
+            p01 = _mm_add_epi32(p01, mm_round);
+            pRG1 = _mm_srli_epi32(pRG1, 8);      // /256
+            pRG2 = _mm_srli_epi32(pRG2, 8);      // /256
+            p01 = _mm_srli_epi32(p01, 8);      // /256
+
+            // convert to 16bit
+            pRG1 = _mm_packus_epi32(pRG1, pRG2); //R0G0B0R1G1B1R2G2
+            p01 = _mm_packus_epi32(p01, p01); //B2R3B3G3
+            pRG1 = _mm_packus_epi16(pRG1, mm_zeros);
+            p01 = _mm_packus_epi16(p01, mm_zeros);
+            _mm_storeu_si128((__m128i *)pdst, _mm_unpacklo_epi64(pRG1, p01));
+            pdst += 12;
+        }
+        for (; x < dwidth; x++) {
+            const unsigned char *p0 = pSrc1 + Xmap[x];
+            const unsigned char *p1 = pSrc2 + Xmap[x];
+            *pdst++ = ((Xf1[x] * fy1*p0[0]) + (Xf[x] * fy1*p0[3]) + (Xf1[x] * fy*p1[0]) + (Xf[x] * fy*p1[3]) + 0x8000) >> 16;
+            *pdst++ = ((Xf1[x] * fy1*p0[1]) + (Xf[x] * fy1*p0[4]) + (Xf1[x] * fy*p1[1]) + (Xf[x] * fy*p1[4]) + 0x8000) >> 16;
+            *pdst++ = ((Xf1[x] * fy1*p0[2]) + (Xf[x] * fy1*p0[5]) + (Xf1[x] * fy*p1[2]) + (Xf[x] * fy*p1[5]) + 0x8000) >> 16;
+        }
+    }
+}
+
 
 void InferenceEngine::DecodeScaleAndConvertToTensorBatch(std::vector<std::tuple<char*, int>>& batch_Q, int start, int end, int dim[3], float *tens_buf)
 {
