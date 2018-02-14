5e55b05a67274bed95d7be72d1097864e37af3da
Radha Giduthuri <rgiduthuri@gmail.com>
2018-02-14 13:06:07 -0800

    upgraded fully connected and softmax to accept 2D tensors (#160)

--- vx_nn/src/softmax_layer.cpp
+++ vx_nn/src/softmax_layer.cpp
@@ -39,28 +39,26 @@ static vx_status VX_CALLBACK validateSoftmaxLayer(vx_node node, const vx_referen
     // check tensor dimensions
     vx_enum type;
     vx_size num_dims;
-    vx_size input_dims[4], output_dims[4];
+    vx_size input_dims[4] = { 1, 1, 1, 1 }, output_dims[4] = { 1, 1, 1, 1 };
     ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor)parameters[0], VX_TENSOR_NUMBER_OF_DIMS, &num_dims, sizeof(num_dims)));
     ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor)parameters[0], VX_TENSOR_DATA_TYPE, &type, sizeof(type)));
-    if(num_dims != 4) return VX_ERROR_INVALID_DIMENSION;
+    if(num_dims != 2 && num_dims != 4) return VX_ERROR_INVALID_DIMENSION;
     if(type != VX_TYPE_FLOAT32) return VX_ERROR_INVALID_TYPE;
-    ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor)parameters[0], VX_TENSOR_DIMS, input_dims, sizeof(input_dims)));
+    ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor)parameters[0], VX_TENSOR_DIMS, &input_dims[4-num_dims], num_dims*sizeof(vx_size)));
     ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor)parameters[1], VX_TENSOR_NUMBER_OF_DIMS, &num_dims, sizeof(num_dims)));
     ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor)parameters[1], VX_TENSOR_DATA_TYPE, &type, sizeof(type)));
-    if(num_dims != 4) return VX_ERROR_INVALID_DIMENSION;
+    if(num_dims != 2 && num_dims != 4) return VX_ERROR_INVALID_DIMENSION;
     if(type != VX_TYPE_FLOAT32) return VX_ERROR_INVALID_TYPE;
-    ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor)parameters[1], VX_TENSOR_DIMS, output_dims, sizeof(output_dims)));
+    ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor)parameters[1], VX_TENSOR_DIMS, &output_dims[4-num_dims], num_dims*sizeof(vx_size)));
     if(output_dims[3] != input_dims[3]) return VX_ERROR_INVALID_DIMENSION;
     if(output_dims[2] != input_dims[2]) return VX_ERROR_INVALID_DIMENSION;
     if(output_dims[1] != input_dims[1]) return VX_ERROR_INVALID_DIMENSION;
     if(output_dims[0] != input_dims[0]) return VX_ERROR_INVALID_DIMENSION;
 
     // output tensor configuration
-    type = VX_TYPE_FLOAT32;
-    num_dims = 4;
     ERROR_CHECK_STATUS(vxSetMetaFormatAttribute(metas[1], VX_TENSOR_DATA_TYPE, &type, sizeof(type)));
     ERROR_CHECK_STATUS(vxSetMetaFormatAttribute(metas[1], VX_TENSOR_NUMBER_OF_DIMS, &num_dims, sizeof(num_dims)));
-    ERROR_CHECK_STATUS(vxSetMetaFormatAttribute(metas[1], VX_TENSOR_DIMS, output_dims, sizeof(output_dims)));
+    ERROR_CHECK_STATUS(vxSetMetaFormatAttribute(metas[1], VX_TENSOR_DIMS, &output_dims[4-num_dims], num_dims*sizeof(vx_size)));
     return VX_SUCCESS;
 }
 
