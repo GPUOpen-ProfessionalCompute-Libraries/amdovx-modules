a6bd41762d6ab0b09c7e62b285b09681602bc91e
Radha Giduthuri <rgiduthuri@gmail.com>
2018-04-29 09:59:01 -0700

    nnir reshape

--- utils/model_compiler/python/nnir2openvx.py
+++ utils/model_compiler/python/nnir2openvx.py
@@ -542,6 +542,14 @@ VX_API_ENTRY vx_status VX_API_CALL annAddToGraph(vx_graph graph, %s, %s, const c
       ERROR_CHECK_STATUS(vxReleaseNode(&node));
     }
 """ % (node.inputs[0], node.outputs[0]))
+            elif node.type == 'reshape':
+                f.write( \
+"""
+    { vx_node node = vxReshapeLayer(graph, %s, %s);
+      ERROR_CHECK_OBJECT(node);
+      ERROR_CHECK_STATUS(vxReleaseNode(&node));
+    }
+""" % (node.inputs[0], node.outputs[0]))
             else:
                 raise ValueError("Unsupported node by OpenVX: {}".format(node.type))
         f.write( \
